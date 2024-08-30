7b96f77f785fc69f520896714c00b7b0ac84a4a5
NAHID12105080 <nahid151341@gmail.com>
2024-08-30 17:29:27 +0600

    remove next-video and used html5 video component

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
 import { getTodos } from "./actions/actions";
 import TodoList from "./components/TodoList";
 import AddTodoForm from "./components/AddTodoForm";
-import Video from "next-video";
-import backgroundVideo from "@/videos/background-video.mp4";
+import VideoBackground from "./components/VideoBackground";
 
 export default async function Home() {
   const todos = await getTodos();
 
   return (
     <>
-      <main className="max-w-4xl mx-auto mt-4 relative z-40">
-        <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+      <main className="max-w-2xl mx-auto mt-4 p-6 relative ">
+        <h1 className="text-3xl font-bold mb-6 text-gray-800">Todo List</h1>
+
         <AddTodoForm />
         <TodoList initialTodos={todos} />
       </main>
-      <Video
-        src={backgroundVideo}
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="absolute top-0 left-0 w-full h-full object-cover -z-40"
-      />
     </>
   );
 }
